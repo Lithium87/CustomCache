4f9e0275253af97ec92c719870ddb66557999887
Lithium87 <victoriavandeva@gmail.com>
2026-02-04 21:48:35 +0200

    Add project files.

---
+++ CustomCache/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CustomCache
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataDownloader dataDownloader = new SlowDataDownloader();
+
+            Console.WriteLine(dataDownloader.DownloadData("id1"));
+            Console.WriteLine(dataDownloader.DownloadData("id2"));
+            Console.WriteLine(dataDownloader.DownloadData("id3"));
+            Console.WriteLine(dataDownloader.DownloadData("id1"));
+            Console.WriteLine(dataDownloader.DownloadData("id3"));
+            Console.WriteLine(dataDownloader.DownloadData("id1"));
+            Console.WriteLine(dataDownloader.DownloadData("id2"));
+        }
+    }
+
+    public interface IDataDownloader
+    {
+        string DownloadData(string resourceId);
+    }
+
+    public class SlowDataDownloader : IDataDownloader
+    {
+        public string DownloadData(string resourceId)
+        {
+            //let's imagine this method downloads real data,
+            //and it does it slowly
+            Thread.Sleep(1000);
+            return $"Some data for {resourceId}";
+        }
+    }
+}
