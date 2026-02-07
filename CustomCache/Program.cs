632e45984412272214cdb539cb549e009ff5ea9e
Lithium87 <victoriavandeva@gmail.com>
2026-02-07 16:48:49 +0200

    Implement Cache class

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -16,6 +16,21 @@
         }
     }
 
+    public class Cache<TKey, TData>
+    {
+        private readonly Dictionary<TKey, TData> _cachedData = new ();
+
+        public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+        {
+            if(!_cachedData.ContainsKey(key))
+            {
+                _cachedData[key] = getForTheFirstTime(key); 
+            }
+
+            return _cachedData[key];
+        }
+    }
+
     public interface IDataDownloader
     {
         string DownloadData(string resourceId);
@@ -23,7 +38,14 @@
 
     public class SlowDataDownloader : IDataDownloader
     {
+        private readonly Cache<string, string> _cache = new ();
+
         public string DownloadData(string resourceId)
+        {
+            return _cache.Get(resourceId, DownloadDataWithoutCaching);
+        }
+
+        private string DownloadDataWithoutCaching(string resourceId)
         {
             //let's imagine this method downloads real data,
             //and it does it slowly
